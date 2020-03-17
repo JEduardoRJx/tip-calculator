b2b89a29de15a1b73eec269980d244fd695cfbaf
Eduardo <jeduardorjx@gmail.com>
2020-03-16 19:41:32 -0600

    Disable the buttons if the inputs are empty

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ function handlePageLoad() {
   calcBtnStatus();
 }
 
+function calcBtnStatus() {
+  let billAmount = parseFloat(billAmountInput.value)
+  let numOfPeople = parseFloat(numOfPeopleInput.value)
+  if ( billAmount > 0 && numOfPeople > 0) {
+    calcBtn.disabled = false;
+    clearBtn.disabled = false
+  } else {
+    calcBtn.disabled = true;
+    clearBtn.disabled = true;
+  }
+}
