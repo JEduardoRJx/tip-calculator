b1d8bbd399a3cb8017dbc933b609a7343d59e59a
Eduardo <jeduardorjx@gmail.com>
2020-03-16 19:56:58 -0600

    Clear the inputs and a the tip amount if there is one

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.addEventListener('load', handlePageLoad);
 billAmountInput.addEventListener('keyup', calcBtnStatus);
 numOfPeopleInput.addEventListener('keyup', calcBtnStatus);
 calcBtn.addEventListener('click', calcTip);
-clearBtn.addEventListener('click', clearInputs)
+clearBtn.addEventListener('click', clearInputs);
 
 function handlePageLoad() {
   calcBtnStatus();
@@ -27,12 +27,12 @@ function calcBtnStatus() {
 }
 
 function calcTip() {
-  let billAmount = parseFloat(billAmountInput.value)
-  let numOfPeople = parseFloat(numOfPeopleInput.value)
-  let serviceRating = parseFloat(document.querySelector('select').value)
+  let billAmount = parseFloat(billAmountInput.value);
+  let numOfPeople = parseFloat(numOfPeopleInput.value);
+  let serviceRating = parseFloat(document.querySelector('select').value);
   let total = (billAmount * serviceRating) / numOfPeople;
-  total = Math.round(total * 100) / 100
-  total = total.toFixed(2)
+  total = Math.round(total * 100) / 100;
+  total = total.toFixed(2);
   renderTip(total);
 }
 
@@ -43,11 +43,11 @@ function renderTip(total) {
     <h1 class='tip'>TIP AMOUNT</h1>
     <h1 class='tip'>$${total} Each</h1>
   </div>
-`)
+`);
 }
 
 function clearTip() {
-  let sibiling = document.querySelector('.tip')
+  let sibiling = document.querySelector('.tip');
   if (sibiling) {
     sibiling.parentNode.removeChild(sibiling);
  }
@@ -55,9 +55,9 @@ function clearTip() {
 
 function clearInputs() {
   let serviceRating = document.querySelector('select')
-  serviceRating.value = 'None'
-  billAmountInput.value = ''
-  numOfPeopleInput.value = ''
-  calcBtnStatus()
+  serviceRating.value = '--';
+  billAmountInput.value = '';
+  numOfPeopleInput.value = '';
+  calcBtnStatus();
   clearTip();
 }
