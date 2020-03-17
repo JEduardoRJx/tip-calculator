26b7d4c86a0c977ee146aa92b8cd0aed33467f24
Eduardo <jeduardorjx@gmail.com>
2020-03-16 19:39:57 -0600

    Add event listener to the window for when the page loads

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let billAmountInput = document.querySelector('.amount')
 let numOfPeopleInput = document.querySelector('.numofpeople')
 let calcBtn = document.querySelector('.calc-btn')
 let clearBtn = document.querySelector('.clear-btn')
+
+window.addEventListener('load', handlePageLoad);
+
+
+function handlePageLoad() {
+  
+}
